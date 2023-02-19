c921b53f8059204b30d25f8c884eefc5e06f5bea
David Adjirackor <adjirackor93@gmail.com>
2023-02-19 15:43:14 +0000

    print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,14 +15,13 @@ int main(void)
 	while (e < 10)
 	{
 		d = 0;
-		while (c < 10)
+		while (d < 10)
 		{
 			c = 0;
 			while (c < 10)
 			{
 				if (c != d && d != e && e < d && d < c)
 				{
-
 					putchar('0' + e);
 					putchar('0' + d);
 					putchar('0' + c);
