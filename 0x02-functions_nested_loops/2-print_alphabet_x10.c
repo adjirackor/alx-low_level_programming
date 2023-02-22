ae7aed2b6a2df5b17a97d5a43b71658d052944d0
David Adjirackor <adjirackor93@gmail.com>
2023-02-22 00:47:11 +0000

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,15 @@
  * Return: Always 0
  */
 
-void print_alphabet_x10 (void)
+void print_alphabet_x10(void)
 {
 	char x;
 	int y;
-	for(y = 1; y <= 10; y++)
+
+	for (y = 1; y <= 10; y++)
 
 	{
-		for (x = 'a';x <= 'z'; x++)
+		for (x = 'a'; x <= 'z'; x++)
 			_putchar(x);
 		_putchar('\n');
 
