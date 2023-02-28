4c2ddc9f51d1b1636cdebba31c10119ce9dad18f
David Adjirackor <adjirackor93@gmail.com>
2023-02-28 02:00:11 +0000

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - takes a pointer value and updates it to 98
+ * @n: a pointer to int
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains the project tasks of pointers, arrays and strings.
