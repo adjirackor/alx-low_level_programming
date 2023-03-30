082169c1fa2c3298c22f09401aa5f8e0a2083f67
David Adjirackor <adjirackor93@gmail.com>
2023-03-30 22:57:46 +0000

    task zero

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
 /**
   * struct list_s - singly linked list
-  * @str: string 
+  * @str: string
   * @len: length of string
   * @next: pointer to next node
   *
@@ -18,8 +19,10 @@ typedef struct list_s
 } list_t;
 
 
-#include <stddef.h>
-
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head)
 
 #endif /* LISTS_H */
