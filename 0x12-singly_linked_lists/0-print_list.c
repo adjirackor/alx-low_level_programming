7b251a29339b46d4fbf3d6b22aee66173088eaeb
David Adjirackor <adjirackor93@gmail.com>
2023-03-30 22:43:04 +0000

    first commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * print_list - prints all the elements of a list 
+ * @h: pointer to the head of the list 
+ *
+ * Return: the number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t x = 0;
+	const list_t *hot = h;
+
+	while (hot != NULL)
+	{
+		if (hot->str != NULL)
+		{
+			printf("%d %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		hot = hot->next;
+		x++;
+	}
+	return (x);
+}
