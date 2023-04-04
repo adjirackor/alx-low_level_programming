a2b85d95f7a85fda3fd94b0db0f53c7f77d32911
David Adjirackor <adjirackor93@gmail.com>
2023-04-04 10:34:27 +0000

    task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,6 +14,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *last = *head;
 
 	new_node = malloc(sizeof(listint_t));
+	
+	if (new_node == NULL)
+		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
 
