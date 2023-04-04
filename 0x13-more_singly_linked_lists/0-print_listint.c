ecca9f23c1230cb1339d28f46856c8801cab55cc
David Adjirackor <adjirackor93@gmail.com>
2023-04-04 07:04:52 +0000

    correction to first commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * print_listint - prints all the elements of a list 
+ * print_listint - prints all the elements of a list
  * @h: pointer to the head node
  * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-        size_t count = 0;
+	size_t count = 0;
 
-        while (h != NULL)
-        {
-                printf("%d\n", h->n);
-                h = h->next;
-                count++;
-        }
-        return (count);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+
 }
 
