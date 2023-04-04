4e9ff4a92ec2c962c5a1d692b2cf92dd291fea50
David Adjirackor <adjirackor93@gmail.com>
2023-04-04 11:18:26 +0000

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,6 +10,9 @@ void free_listint(listint_t *head)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
 
-	free(new_node);
-	free(head);
-}
+	while (head != NULL)
+	{
+		new_node = head;
+		head = head->next;
+		free(new_node);
+	}
